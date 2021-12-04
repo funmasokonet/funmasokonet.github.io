b7a172c0f2ba3e352e1163f6c17b1b8bd14f9cd0
Masoko <jeleff@gmail.com>
2021-12-04 13:40:18 +0000

    update

--- update.sh
+++ update.sh
@@ -1,14 +1,12 @@
 echo starting pelican
-cd /home/pi/test/test
+cd /home/pi/git/funmasokonet.github.io/
 /home/pi/sites/pelican/bin/pelican content
 echo done with pelican
 now=$(date)
 msg="New Joke - $now"
 cd output/
-git checkout gh-pages
 git add -A .
 git commit -a -m "$msg"
 git push origin gh-pages
 cd ..
-git checkout main
 echo done
