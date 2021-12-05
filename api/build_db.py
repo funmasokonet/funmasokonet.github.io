d61ccec3ca804fa78f3d575855562fb303b65073
Hristo Jelev <jeleff@gmail.com>
2021-12-05 22:28:03 +0200

    api update

--- api/build_db.py
+++ api/build_db.py
@@ -7,19 +7,28 @@ from flask import Flask, jsonify
 # configuration variables for pulling the jokes from the site
 content_folder = os.getcwd().replace("api", "content/posts")
 pattern = "*.md"
-
+jokes = []
+categories = [{'bg': "футбол", 'en': "football"}, {'bg': "работа", 'en': "work"}, {'bg': "разни", 'en': "others"},
+              {'bg': "семейни", 'en': "family"}, {'bg': "училище", 'en': "school"},
+              {'bg': "за иванчо", 'en': "ivancho"}, {'bg': "за големи", 'en': "nsfw"}]
 app = Flask(__name__)
-
+hits = 0
 
 @app.route("/")
 def just_joke():
+    global hits
+    hits += 1
     return get_random_joke().text
 
+
 @app.route("/count")
+@app.route("/count/")
 def count_jokes():
-    return "{}  вица в нашата база. \n".format(len(jokes))
+    return "{}  вица в нашата база. \n{} hits.\n".format(len(jokes), hits)
+
 
 @app.route("/update")
+@app.route("/update/")
 def update():
     update_jokes_list()
     return "Jokes DB updated\n"
@@ -28,14 +37,18 @@ def update():
 @app.route("/html/")
 @app.route("/html")
 def json_joke():
+    global hits
+    hits += 1
     return "<br />".join(get_random_joke().text.split("\n"))
 
 
-@app.route("/category/", defaults={'cat_en': None, 'html':False})
+@app.route("/category/", defaults={'cat_en': None, 'html': False})
 @app.route("/<html>/category/", defaults={'cat_en': None})
-@app.route("/category/<cat_en>", defaults={ 'html':False})
-@app.route("/<html>/category/<cat_en>")
-def category_joke(html,cat_en):
+@app.route("/category/<cat_en>/", defaults={'html': False})
+@app.route("/<html>/category/<cat_en>/")
+def category_joke(html, cat_en):
+    global hits
+    hits += 1
     if cat_en == None:
         cat = []
         for c in categories:
@@ -50,12 +63,6 @@ class joke:
     def __init__(self, text, category):
         self.text = text
         self.category = category
-
-
-jokes = []
-categories = [{'bg': "футбол", 'en': "football"}, {'bg': "работа", 'en': "work"}, {'bg': "разни", 'en': "others"},
-              {'bg': "семейни", 'en': "family"}, {'bg': "училище", 'en': "school"},
-              {'bg': "за иванчо", 'en': "ivancho"}, {'bg': "за големи", 'en': "nsfw"}]
 
 
 def get_file_names(folder, mask):
@@ -76,7 +83,8 @@ def get_jokes(joke_files):
             for line in lines:
                 if "Category:" in line:
                     joke_cat = line.split(":")[1].strip()
-                elif ("Title:" in line) or ("Date:" in line) or ("Tags:" in line) or (line.strip() == "") or (re.match(r'^\s*$', line)):
+                elif ("Title:" in line) or ("Date:" in line) or ("Tags:" in line) or (line.strip() == "") or (
+                re.match(r'^\s*$', line)):
                     pass
                 else:
                     joke_text.append(line)
