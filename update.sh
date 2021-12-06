9d3d74c35a4835a69d264f5ddb5410a22e65372a
Masoko <jeleff@gmail.com>
2021-12-06 17:30:33 +0000

    New Joke - Mon Dec  6 17:30:33 GMT 2021

--- update.sh
+++ update.sh
@@ -1,17 +1,17 @@
 now=$(date)
 msg="New Joke - $now"
-echo commiting main branch
+echo -----------------  commiting main branch
 cd /home/pi/git/funmasokonet.github.io/
 git add .
 git commit -a -m "$msg"
 git push origin main
-echo starting pelican
+echo -----------------  starting pelican
 /home/pi/sites/pelican/bin/pelican content
 echo done with pelican
 cd output/
-echo commiting gh-pages branch
+echo -----------------  commiting gh-pages branch
 git add -A .
 git commit -a -m "$msg"
 git push origin gh-pages
 cd ..
-echo done
+echo -----------------  done
